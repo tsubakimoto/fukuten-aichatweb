646877d527767f0f7cd7837d17ce12c6c9df480f
Yuta Matsumura <tkmbmbr@gmail.com>
2025-12-14 11:28:19 +0900

    Cosmso DB統合の追加

--- src/ChatApp.AppHost/AppHost.cs
+++ src/ChatApp.AppHost/AppHost.cs
@@ -6,8 +6,19 @@ var builder = DistributedApplication.CreateBuilder(args);
 var model = GitHubModel.OpenAI.OpenAIGpt4oMini;
 var chat = builder.AddGitHubModel("chat", model);
 
+// Azure Cosmos DB
+#pragma warning disable ASPIRECOSMOSDB001
+var cosmos = builder.AddAzureCosmosDB("cosmos-db").RunAsPreviewEmulator(emulator =>
+{
+    emulator.WithDataExplorer();
+});
+var db = cosmos.AddCosmosDatabase("db");
+var container = db.AddContainer("chats", "/id");
+
+// ChatApp Project
 var web = builder.AddProject<Projects.ChatApp>("chatapp")
-                 .WithReference(chat);
+                 .WithReference(chat)
+                 .WithReference(container);
 
 // Dev tunnels
 //   https://learn.microsoft.com/en-us/azure/developer/dev-tunnels/get-started?tabs=windows
