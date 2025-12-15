1e1a6ae2da433850a2b32f09401588c13982a881
Yuta Matsumura <tkmbmbr@gmail.com>
2025-12-15 23:25:42 +0900

    リファクタリング

--- src/ChatConsole/Program.cs
+++ src/ChatConsole/Program.cs
@@ -8,15 +8,14 @@ string key = Environment.GetEnvironmentVariable("CHAT_KEY")
     ?? throw new InvalidOperationException("Missing configuration: CHAT_KEY.");
 string endpoint = Environment.GetEnvironmentVariable("CHAT_URI")
     ?? throw new InvalidOperationException("Missing configuration: CHAT_URI.");
+string deployment = Environment.GetEnvironmentVariable("CHAT_MODEL")
+    ?? throw new InvalidOperationException("Missing configuration: CHAT_MODEL.");
 
 ApiKeyCredential credential = new(key);
 OpenAIClientOptions openAIOptions = new OpenAIClientOptions()
 {
     Endpoint = new Uri(endpoint)
 };
-
-string deployment = Environment.GetEnvironmentVariable("CHAT_MODEL")
-    ?? throw new InvalidOperationException("Missing configuration: CHAT_MODEL.");
 IChatClient client = new OpenAIClient(credential, openAIOptions).GetChatClient(deployment).AsIChatClient();
 
 string prompt = "What is the capital of Japan?";
